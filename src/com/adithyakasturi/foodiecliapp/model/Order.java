f4bb96a9b6adecc55ec0ce90b325bfa3918fb4ef
Adithya Kasturi <kasturiadithya44@gmail.com>
2024-08-10 14:47:12 +0530

    feat: Add Model Classes for Customer,Dishes,Restaurant

---
+++ src/com/adithyakasturi/foodiecliapp/model/Order.java
@@ -0,0 +1,90 @@
+package com.adithyakasturi.foodiecliapp.model;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+public class Order {
+    private String id;
+    private Customer customer;
+    private Restaurant restaurant;
+    private List<Dish> dishes;
+    private double price;
+    private Date orderdate;
+
+    public Order() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public List<Dish> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(List<Dish> dishes) {
+        this.dishes = dishes;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getOrderdate() {
+        return orderdate;
+    }
+
+    public void setOrderdate(Date orderdate) {
+        this.orderdate = orderdate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Double.compare(price, order.price) == 0 && Objects.equals(id, order.id) && Objects.equals(customer, order.customer) && Objects.equals(restaurant, order.restaurant) && Objects.equals(dishes, order.dishes) && Objects.equals(orderdate, order.orderdate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, customer, restaurant, dishes, price, orderdate);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id='" + id + '\'' +
+                ", customer=" + customer +
+                ", restaurant=" + restaurant +
+                ", dishes=" + dishes +
+                ", price=" + price +
+                ", orderdate=" + orderdate +
+                '}';
+    }
+}
